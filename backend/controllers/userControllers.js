875452a83b1635ef84f7b8fd1421636284abde76
KHSenevirathne <khsenevirathne@gmail.com>
2024-06-08 19:54:50 +0530

    Add User

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,18 @@
+import User from "../models/userModel.js";
+import asyncHandler from "../middlewares/asyncHandler.js";
+
+const createUser = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+    
+    if (!username || !email || !password) {
+        throw new Error("Please fill all the inputs.");
+      }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) res.status(400).send("User already exists");
+
+    const newUser = new User({ username, email, password });
+
+});
+
+export { createUser };
