e186049a6a86857506c0a1eddd86b108874aae15
KHSenevirathne <khsenevirathne@gmail.com>
2024-06-11 21:57:56 +0530

    Admin CRUD

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {createUser, loginUser, logoutCurrentUser, getAllUsers, getCurrentUserProfile, updateCurrentUserProfile} from "../controllers/userControllers.js";
+import {createUser, loginUser, logoutCurrentUser, getAllUsers, getCurrentUserProfile, updateCurrentUserProfile, deleteUserById, getUserById, updateUserById} from "../controllers/userControllers.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
@@ -8,5 +8,12 @@ router.route("/").post(createUser).get(authenticate, authorizeAdmin, getAllUsers
 router.post("/auth", loginUser);
 router.post("/logout", logoutCurrentUser);
 router.route("/profile").get(authenticate, getCurrentUserProfile).put(authenticate, updateCurrentUserProfile);
+
+// ADMIN ROUTES
+router
+  .route("/:id")
+  .delete(authenticate, authorizeAdmin, deleteUserById)
+  .get(authenticate, authorizeAdmin, getUserById)
+  .put(authenticate, authorizeAdmin, updateUserById);
 
 export default router;
