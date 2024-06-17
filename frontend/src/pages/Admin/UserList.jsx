4b1c0b8f87a86927b42935347046e6c49d498f43
KHSenevirathne <khsenevirathne@gmail.com>
2024-06-17 07:17:44 +0530

    UserList Update

--- frontend/src/pages/Admin/UserList.jsx
+++ frontend/src/pages/Admin/UserList.jsx
@@ -59,7 +59,7 @@ const UserList = () => {
 
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-semibold mb-4">Users</h1>
+      <h1 className="text-2xl font-semibold mb-4 ml-[10%]">Users</h1>
       {isLoading ? (
         <Loader />
       ) : error ? (
